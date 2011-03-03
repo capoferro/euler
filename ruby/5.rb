dbe8dc61963349e9237c02b1d6aac138c5287abc
Josiah Kiehl <bluepojo@gmail.com>
2011-03-02 22:16:24 -0500

    Problem 5 - ruby ... barely under 1 minute, could use some work

---
+++ ruby/5.rb
@@ -0,0 +1,32 @@
+# 2520 is the smallest number that can be divided by each of the
+# numbers from 1 to 10 without any remainder.
+
+# What is the smallest positive number that is evenly divisible by all
+# of the numbers from 1 to 20?
+
+class Integer
+  
+  def factor_of? n
+    n % self == 0
+  end
+  
+end
+
+i = 40
+while true
+  divisible = true
+  20.downto(11).each do |k|
+    divisible = false unless i % k == 0
+  end
+  if divisible
+    puts i
+    break
+  end
+  i += 10
+end
+
+# This one could use a touch of optimization. :)
+
+# real	0m59.624s
+# user	0m58.688s
+# sys	0m0.044s
