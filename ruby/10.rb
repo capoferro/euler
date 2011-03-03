4cf10cf7d7a432adb0fcd1207ef7440a6d6d97e8
Josiah Kiehl <bluepojo@gmail.com>
2011-03-03 13:10:08 -0500

    Problem 10 - ruby

---
+++ ruby/10.rb
@@ -0,0 +1,17 @@
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+# Find the sum of all the primes below two million.
+
+class Integer
+
+  def prime?
+    (2..Math.sqrt(self).ceil).each do |i|
+      return false if self % i == 0
+    end
+    true
+  end
+
+end
+
+puts (1..2000000).select(&:prime?).inject(&:+)
+
