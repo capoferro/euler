9efc6ec55bfbc40768ba5f39c79e5a598beae835
Josiah Kiehl <bluepojo@gmail.com>
2011-03-02 22:30:08 -0500

    Save 15sec by going 11 to 20

--- ruby/5.rb
+++ ruby/5.rb
@@ -7,16 +7,10 @@
 i = 40
 while true
   divisible = true
-  20.downto(11).each { |k| divisible = false unless i % k == 0 }
+  (11..20).each { |k| divisible = false unless i % k == 0 }
   if divisible
     puts i
     break
   end
   i += 10
 end
-
-# This one could use a touch of optimization. :)
-
-# real	0m59.624s
-# user	0m58.688s
-# sys	0m0.044s
