a4ca0f1ad678e84add1a80daa26b746a49211d39
Josiah Kiehl <bluepojo@gmail.com>
2011-04-27 15:14:16 -0400

    Improved 1.clj

--- clojure/1.clj
+++ clojure/1.clj
@@ -2,7 +2,11 @@
 
 ;; Find the sum of all the multiples of 3 or 5 below 1000.
 
-(def n 0)
-(doseq [i (range 1 1000)]
-  (if (or (= (rem i 3) 0) (= (rem i 5) 0)) (def n (+ n i))))
-(println n)
+(defn sum
+  [limit]
+  (reduce + (filter #(or (zero? (rem % 3))
+                         (zero? (rem % 5)))
+                    (take limit (iterate inc 1)))))
+
+(println (sum 999))
+
